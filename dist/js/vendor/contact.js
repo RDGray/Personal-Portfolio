f8e2542f88a4838c48bf8bb4cb7447ff8bdb7d2d
RDGray <70549101+RDGray@users.noreply.github.com>
2021-09-20 18:20:18 +0300

    Contact form email
    js for Contact me section. Linked with the email

---
+++ dist/js/vendor/contact.js
@@ -0,0 +1,33 @@
+let fname = document.getElementById("from_name");
+let femail = document.getElementById("from_email");
+let mess = document.getElementById("message");
+let frm = document.getElementById("contact-form");
+
+(function () {
+  emailjs.init("user_E1Uw6QGLoJLD1N6tztxYL");
+})();
+
+window.onload = function () {
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = (Math.random() * 100000) | 0;
+      // these IDs from the previous steps
+      emailjs.sendForm("service_rdgan", "template_cr5qvre", this).then(
+        function () {
+          alert("message sent!");
+
+          fname.value = " ";
+          femail.value = " ";
+          mess.value = " ";
+        },
+        function (error) {
+          alert("FAILED...", error);
+        }
+      );
+    });
+};
+
+console.log((femail.value = ""));
